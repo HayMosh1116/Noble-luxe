c7076bd7f4c98b37fb8816a8baf9a1da93b14bed
‎⁨👾𝒟𝐸𝒱-𝐻𝒜𝒴𝒲𝐻𝒴//𝒯𝐸𝒞𝐻🤖⁩ <Ibrahimfridaous11@gmail.com>
2026-09-14 21:22:40 +0100

    Update clerkProxyMiddleware.ts

--- artifacts/api-server/src/middlewares/clerkProxyMiddleware.ts
+++ artifacts/api-server/src/middlewares/clerkProxyMiddleware.ts
@@ -23,7 +23,7 @@ import type { IncomingHttpHeaders } from 'http';
 import type { RequestHandler } from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const CLERK_FAPI = 'https://clerk.nobleluxe18.com.ng';
+const CLERK_FAPI = "https://frontend-api.clerk.dev";
 export const CLERK_PROXY_PATH = '/api/__clerk';
 
 /**
